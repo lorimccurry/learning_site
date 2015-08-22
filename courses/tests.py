34a358e1475015b726353463fcbbb1d580f37a31
Lori McCurry <lori.mccurry@gmail.com>
2015-08-22 10:57:00 -0500

    Adding template tests

--- courses/tests.py
+++ courses/tests.py
@@ -50,12 +50,16 @@ class CourseViewsTest(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertIn(self.course, resp.context['courses'])
         self.assertIn(self.course2, resp.context['courses'])
+        self.assertTemplateUsed(resp, 'courses/course_list.html')
+        self.assertContains(resp, self.course.title)
 
     def test_course_detail_view(self):
         resp = self.client.get(reverse('courses:course',
                                         kwargs={'pk': self.course.pk}))
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(self.course, resp.context['course'])
+        self.assertTemplateUsed(resp, 'courses/course_detail.html')
+        self.assertContains(resp, self.course.title)
 
     def test_step_detail_view(self):
         resp = self.client.get(reverse('courses:step',
@@ -63,3 +67,6 @@ class CourseViewsTest(TestCase):
                         'step_pk': self.step.pk}))
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(self.step, resp.context['step'])
+        self.assertTemplateUsed(resp, 'courses/step_detail.html')
+        self.assertContains(resp, self.course.title)
+        self.assertContains(resp, self.step.title)
